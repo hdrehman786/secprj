e5b134a05348dd10d91a847f89cb26382f7ebe11
HD Rehman <161239883+hdrehman786@users.noreply.github.com>
2025-02-09 08:48:15 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://secprj-d6eq.vercel.app/",
+    origin: "https://secprj-d6eq.vercel.app",
     credentials: true
 }))
 connectDB();
